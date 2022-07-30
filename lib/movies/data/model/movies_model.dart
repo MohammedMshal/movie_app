080b651406a473ea99c059dbd1b753aee38b3f43
MohammedMshal <mohammedmshal2018@gmail.com>
2022-07-30 05:49:20 +0200

    modle data layer

---
+++ lib/movies/data/model/movies_model.dart
@@ -0,0 +1,23 @@
+import 'package:clean_arctitcher/movies/domain/entities/movie.dart';
+
+class MoviesModel extends Movie {
+  const MoviesModel({
+    required super.id,
+    required super.title,
+    required super.backdropPath,
+    required super.genreIds,
+    required super.overView,
+    required super.voteAverage,
+    required super.releaseDate,
+  });
+
+  factory MoviesModel.fromJson(Map<String, dynamic> json) => MoviesModel(
+        id: json['id'],
+        title: json['title'],
+        backdropPath: json['backdrop_path'],
+        genreIds: List<int>.from(json['genre_ids'].map((e) => e)),
+        overView: json['overview'],
+        voteAverage: json['vote_average'],
+        releaseDate: json['release_date'],
+      );
+}
